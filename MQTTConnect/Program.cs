fd3fe614b058fa7b2f4c83d2d13dd796944c6770
Elias Hasnat <android.hasnat@gmail.com>
2019-05-20 16:06:04 +0900

    mqtt connect

--- MQTTConnect/Program.cs
+++ MQTTConnect/Program.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace MQTTConnect
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("MQTT Connect");
+            // args
+            var iothubName = "vernemq";
+            var hostName = $"{iothubName}.azure-devices.net";
+            var deviceId = "dev1";
+            var sharedAccessKey = "";
+            var port = 8883;
+
+            var deviceCertificateBytes = File.ReadAllBytes($"D:\\Certs\\{deviceId}.pfx");
+            var deviceCertificatePassword = "1234";
+            var deviceCertificate = (X509Certificate2)new X509Certificate2(deviceCertificateBytes, deviceCertificatePassword);
+
+            using (var client = new IoTHubMqttDeviceClient(hostName, deviceId, deviceCertificate, port))
+            {
+                await client.GetTwin((x) =>
+                {
+                    WriteLine($"Received twin: {x}");
+                });
+                await client.DeclareDesiredPropertiesHandler((x) =>
+                {
+                    WriteLine($"Desired Properties: {x}");
+                });
+                await client.DeclareDirectMethodHandler((methodName, methodArgs) =>
+                {
+                    return new
+                    {
+                        A = methodName,
+                        B = methodArgs
+                    };
+                });
+                await client.Cloud2DeviceMessages((p, x) =>
+                {
+                    WriteLine($"MessageReceived: {x}");
+                    WriteLine($"PayloadReceived: {p}");
+                });
+
+                await client.UpdateReportedProperties(new {
+                    r5 = $"{DateTime.Now}",
+                    r6 = $"{DateTime.Now}",
+                }, (x) =>
+                {
+                    Console.Write($"UpdateReportedProperties: {x}");
+                });
+
+                var random = new Random();
+
+                while (true)
+                {
+                    var json = JsonConvert.SerializeObject(new
+                    {
+                        DeviceId = deviceId,
+                        Data = random.Next(0, 100),
+                        DateTime = DateTimeOffset.Now
+                    });
+
+                    await client.SendEventAsString(json);
+
+                    WriteLine(json);
+
+                    await Task.Delay(1000);
+                }
+            }
         }
     }
 }
