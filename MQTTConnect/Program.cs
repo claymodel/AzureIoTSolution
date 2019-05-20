b79ae49a7b095c899afa136084c72027d8c7f846
Elias Hasnat <android.hasnat@gmail.com>
2019-05-20 15:15:54 +0900

    mqtt connect example

---
+++ MQTTConnect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTTConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MQTT Connect");
+        }
+    }
+}
